e68069631c7f547b909d0362dbfb5b7f69260599
Matthieu Napoli <matthieu@mnapoli.fr>
2021-01-28 15:49:51 +0100

    Init

---
+++ tests/AppTest.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Pizza\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class AppTest extends TestCase
+{
+    public function test()
+    {
+        $this->assertTrue(true);
+    }
+}
