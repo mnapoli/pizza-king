aff6bb060bce6bb55ad63626fcd3ad6a4cb052e6
Viktor Szépe <viktor@szepe.net>
2021-01-28 18:39:23 +0100

    Add final line break

--- commande.php
+++ commande.php
@@ -8,4 +8,4 @@
 /*
  * Et ensuite on affiche le prix de la pizza :
  */
-printf("Voici le prix de votre pizza :\n%.2f €", $pizza->prix());
+printf("Voici le prix de votre pizza :\n%.2f €\n", $pizza->prix());
